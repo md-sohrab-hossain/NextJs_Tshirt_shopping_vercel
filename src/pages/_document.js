7621388732a15655ca95ce55273335507f9411d2
sajalkhan <sajalhossen@yahoo.com>
2022-04-11 00:05:15 +0600

    fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,6 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 import 'regenerator-runtime/runtime';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 //!👇 this is the absolute base template for rendering your page
 class CustomeDocument extends Document {
@@ -13,12 +13,37 @@ class CustomeDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta property="custom" content="great work" />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&family=Kanit:wght@200&display=swap"
+            rel="stylesheet"
+          />
         </Head>
 
         <body>
           <Main />
           <NextScript />
+
+          <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossOrigin="anonymous"></script>
+          <script
+            src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+            crossOrigin="anonymous"
+          ></script>
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+            crossOrigin="anonymous"
+          ></script>
         </body>
       </Html>
     );
